4d3a678057bf465992073d598945b43cf3fa97bd
vikaschoudhary007 <vikaschoudhary572001@gmail.com>
2024-05-27 13:30:19 -0400

    added junit tests

--- Java/src/test/java/org/example/CombinedTest.java
+++ Java/src/test/java/org/example/CombinedTest.java
@@ -1,9 +1,6 @@
 package org.example;
-
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -69,6 +66,82 @@ public class CombinedTest {
         assertEquals(pageFaults, calPageFaults, "BlindOracle page faults count is incorrect");
     }
 
+    @Test
+    public void testOverallFunctionality1() {
+        int k = 5;
+        int N = 20;  // N >> k
+        int n = 100;
+        double e = 0.5;
+        double t = 0.2;
+        int w = 7;
 
+        List<Integer> seq = Combined.generateRandomSequence(k, N, n, e);
+        List<Integer> hSeq = Combined.generateH(seq);
+        List<Integer> noisyHSeq = Combined.addNoise(hSeq, t, w);
+
+        int pageFaults = Combined.blindOracle(k, seq, noisyHSeq);
+        System.out.println(
+                "Blind Oracle pageFaults for k:" + k + " N:" + N + " n:" + n + " e:" + e + " t:" + t + " w:" + w + " = " + pageFaults
+        );
+    }
+
+    @Test
+    public void testCombined() {
+        int k = 3;
+        List<Integer> sequence = List.of(1, 2, 3, 4, 1, 2, 5, 1, 2, 3);
+        List<Integer> hSeq = Combined.generateH(sequence);
+        int expectedPageFaults = 10;
+
+        int calculatedPageFaults = Combined.combinedAlg(k, sequence, hSeq, 0);
+
+        assertEquals(expectedPageFaults, calculatedPageFaults, "Combined PageFaults are wrong");
+    }
+
+    @Test
+    public void testLRU() {
+        int k = 4;
+        List<Integer> sequence = List.of(1, 2, 3, 4, 2, 1, 5, 6, 2, 1, 2, 3, 7, 6, 3, 2, 1, 2, 3, 6);
+
+        int pageFaults = Combined.LRU(k, sequence);
+        assertEquals(10, pageFaults, "# pageFaults for k:" + k);
+    }
+
+    @Test
+    public void testOverallLRU() {
+        int k = 5;
+        int N = 6;  // N >> k
+        int n = 100;
+        double e = 0.5;
+        double t = 0.2;
+        int w = 7;
+
+        List<Integer> seq = Combined.generateRandomSequence(k, N, n, e);
+        List<Integer> hSeq = Combined.generateH(seq);
+
+        int pageFaults = Combined.LRU(k, seq);
+        System.out.println(
+                "LRU pageFaults for k:" + k + " N:" + N + " n:" + n + " e:" + e + " t:" + t + " w:" + w + " = " + pageFaults
+        );
+    }
+
+    @Test
+    public void testOverallCombined() {
+        int k = 5;
+        int N = 6;  // N >> k
+        int n = 100;
+        double e = 0.5;
+        double t = 0.2;
+        int w = 7;
+        double thr = 0;
+
+        List<Integer> seq = Combined.generateRandomSequence(k, N, n, e);
+        List<Integer> hSeq = Combined.generateH(seq);
+        List<Integer> noisyHSeq = Combined.addNoise(hSeq, t, w);
+
+        int pageFaults = Combined.combinedAlg(k, seq, noisyHSeq, thr);
+        System.out.println(
+                "Combined pageFaults for k:" + k + " N:" + N + " n:" + n + " e:" + e + " t:" + t + " w:" + w + " = " + pageFaults
+        );
+    }
 
 }
